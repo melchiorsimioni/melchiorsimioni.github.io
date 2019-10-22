cf44fd558591c2b0d070622fb8b404ef21ec04e3
melchiorsimioni <48098455+melchiorsimioni@users.noreply.github.com>
2019-10-22 18:23:53 +0200

    transfo en php

---
+++ apropos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+<link rel="stylesheet", href="css/style.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+
+<meta charset="utf-8" />
+
+<meta name="viewport" content="width=device-width"/>
+
+</head> 
+
+<body>
+
+<p style="margin:20px">La page personnelle <a href="http://melchior.simioni.free.fr">melchior.simioni.free.fr</a> est éditée en HTML et CSS par Melchior Simioni et hébergée sur les serveurs de Free.</p>
+
+<p style="margin:20px">La page <a href="http://melchior.simioni.free.fr">melchior.simioni.free.fr</a> n'utilise pas de cookies.</p>
+
+<p style="margin:20px"> Revenir à la <a href="/">page d'accueil</a> du site. </p>
+
+</body>
+</html>
